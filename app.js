b4a6b116480be6527b54a69f4f9132573b42420e
Bruno Kiafuka <brunokiafuka@live.com.pt>
2024-01-08 20:00:03 +0000

    MEME :)

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
-http.createServer(function (req, res){
- res.write("Mastering the Craft");
- res.end();
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream('index.html').pipe(res);
 }).listen(3000);
 
 console.log("Server running on port 3000");
